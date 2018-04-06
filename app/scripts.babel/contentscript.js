0abc848f245bcd951e76d79a68b43dd4c81f0c9a
rob <rob@robpls.com>
2018-04-05 22:21:10 -0600

    rob.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,11 +1,11 @@
 let author_nodes = Array.from(document.querySelectorAll('.author'));
-let robs_authored_posts = author.filter(author => {
-  author.innerText.indexOf('by robbalvey') > -1
+let robs_authored_posts = author_nodes.filter(author => {
+  return author.innerText.indexOf('by robbalvey') > -1
 });
 
 let threads = Array.from(document.querySelectorAll('.topiclist .row'));
 let robs_threads = threads.filter(thread => {
-  thread.querySelector('dt').innerText.indexOf('by robbalvey') > -1
+  return thread.querySelector('dt').innerText.indexOf('by robbalvey') > -1
 });
 
 for(let post of robs_authored_posts){
