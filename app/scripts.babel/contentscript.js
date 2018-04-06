597ad62397db61d0f975c08c17c148313cf82d5b
rob <rob@robpls.com>
2018-04-05 22:20:03 -0600

    rob help.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,11 +1,17 @@
-let authors = Array.from(document.querySelectorAll('.author')).filter(author => (author.innerText.indexOf('by robbalvey') > -1));
+let author_nodes = Array.from(document.querySelectorAll('.author'));
+let robs_authored_posts = author.filter(author => {
+  author.innerText.indexOf('by robbalvey') > -1
+});
 
-let rows = Array.from(document.querySelectorAll('.topiclist .row')).filter(row => {return row.querySelector('dt').innerText.indexOf('by robbalvey') > -1});
+let threads = Array.from(document.querySelectorAll('.topiclist .row'));
+let robs_threads = threads.filter(thread => {
+  thread.querySelector('dt').innerText.indexOf('by robbalvey') > -1
+});
 
-for(let author of authors){
-	author.parentElement.parentElement.parentElement.remove();
+for(let post of robs_authored_posts){
+	post.parentElement.parentElement.parentElement.remove();
 }
 
-for(let row of rows) {
-	row.remove();
+for(let thread of robs_threads) {
+	thread.remove();
 }
